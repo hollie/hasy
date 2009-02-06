3f6572ad1ac14ac1c80d969bad8b3c914ee15ded
lieven.hollevoet <lieven.hollevoet@365dfac8-a4c5-11dd-9a14-c1645b63b0c6>
2009-02-06 16:05:54 +0000

    Functionality for OneWire readout is now collected in a separate library file.

--- trunk/oswald/firmware/onewire_gateway/onewire_gateway/main.c
+++ trunk/oswald/firmware/onewire_gateway/onewire_gateway/main.c
@@ -6,7 +6,7 @@
 #include "PSoCAPI.h"    // PSoC API definitions for all User Modules
 #include "oo.h"
 
-#define FIRMWARE_VERSION "1.0"
+#define FIRMWARE_VERSION "1.1"
 
 unsigned char id0[8];
 unsigned char id1[8];
@@ -15,12 +15,8 @@ unsigned short temp1;
 
 // Function prototypes
 void hardware_init();
-void print_id(unsigned char *id);
-void get_sensors();
-void get_temp();
 
 #pragma interrupt_handler Ticker_ISR
-
 void Ticker_ISR()
 {
 	LED1_Invert();
@@ -28,40 +24,20 @@ void Ticker_ISR()
 	return;
 }
 
-void print_data()
-{
-	LTRX_PutCRLF();
-	LTRX_CPutString("OneWire gateway v.");
-	LTRX_CPutString(FIRMWARE_VERSION);
-	LTRX_PutCRLF();
-	LTRX_CPutString("Node 1 ID: ");
-	print_id(id0);
-	LTRX_CPutString(" - ");
-	LTRX_PutSHexInt(temp0);
-	LTRX_PutCRLF();
-	LTRX_CPutString("Node 2 ID: ");
-	print_id(id1);
-	LTRX_CPutString(" - ");
-	LTRX_PutSHexInt(temp1);
-	LTRX_PutCRLF();
-	LTRX_CPutString("EOT");
-	LTRX_PutCRLF();
-	
-	return;
-}
+
 void print_header(){
 	LTRX_PutCRLF();
 	LTRX_CPutString("OneWire gateway v.");
 	LTRX_CPutString(FIRMWARE_VERSION);
 	LTRX_PutCRLF();	
-	
+	LTRX_CPutString("Lieven Hollevoet, 2009");
+	LTRX_PutCRLF();	
 	return;
 }
 
 
 void print_help(){
 	print_header();
-	LTRX_PutCRLF();	
 	LTRX_CPutString("Press 'T' to initiate temperature conversion and to get a report");
 	LTRX_PutCRLF();	
 }
@@ -69,25 +45,22 @@ void print_help(){
 void main()
 {
 
-	char * strPtr; // Parameter pointer for UART RX buffer
-	
+	char * UART_bfr; // Parameter pointer for UART RX buffer
 	
 	hardware_init();	
 	
-	get_sensors();
-	get_temp();
-	print_data();
+	print_help();
 	
 	while(1) {
 	
 		// Check if data came in from the Lantronix and reply if required
 		if (LTRX_bCmdCheck()) { // Wait for command
-			if(strPtr = LTRX_szGetParam()) { // More than delimiter?
-				if (*strPtr == '#'){
-					get_sensors();
-					print_data();
-				} else if (*strPtr == 'T'){
+			if(UART_bfr = LTRX_szGetParam()) { // More than delimiter?
+				if (*UART_bfr == 'T'){
+					// Report values of all connected sensors
 					oo_report();
+					LTRX_CPutString("EOT");
+					LTRX_PutCRLF();
 				} else {
 					print_help();
 				}
@@ -121,80 +94,3 @@ void hardware_init(){
 
 }
 
-void get_sensors(){
-
-	char next_found = 0;
-	
-	if (!OneWire_fReset()){
-		LTRX_CPutString("No sensor found on bus");
-		LTRX_PutCRLF();	
-		return;
-	}
-	
-	if (OneWire_fFindFirst()){
-		OneWire_GetROM(id0);
-	} else {
-		LTRX_CPutString("Problem detecting sensor 1");
-		LTRX_PutCRLF();			
-	}
-	if (OneWire_fFindNext()){
-		OneWire_GetROM(id1);
-	} else {
-		LTRX_CPutString("Problem detecting sensor 2");
-		LTRX_PutCRLF();	
-	}
-
-	LTRX_CPutString("get_sensors done...");
-	LTRX_PutCRLF();	
-
-	
-}
-
-void get_temp(){
-
-
-	// Reset
-	if (!OneWire_fReset()){
-		LTRX_CPutString("No sensor found on bus");
-		LTRX_PutCRLF();	
-		return;
-	}
-	
-	// SkipROM
-	OneWire_WriteByte(OO_SKIPROM);
-	
-	// Initiate conversion
-	OneWire_WriteByte(OO_CONVERTT);
-	
-	// Wait until done
-	while (!OneWire_bReadByte()){};
-
-
-	// Select sensor 1 and readout
-	OneWire_fReset();
-	OneWire_SetROM(id0);
-	OneWire_fVerify();
-	OneWire_WriteByte(OO_READPAD);
-	temp0 = OneWire_bReadByte();
-	temp0 += OneWire_bReadByte() << 8;
-	
-	// Select sensor 2 and readout
-	OneWire_fReset();
-	OneWire_SetROM(id1);
-	OneWire_fVerify();
-	OneWire_WriteByte(OO_READPAD);
-	temp1 = OneWire_bReadByte();
-	temp1 += OneWire_bReadByte() << 8;
-
-	LTRX_CPutString("Conversion/grabbing done...");
-	LTRX_PutCRLF();	
-
-}
-
-void print_id(unsigned char *id){
-
-	char cntr = 0;
-	for (cntr=0; cntr<8; cntr++){
-		LTRX_PutSHexByte(*(id+cntr));	
-	}
-}
