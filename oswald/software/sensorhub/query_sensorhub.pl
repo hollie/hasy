23eb1dda8da4051664dc39c9a8bfb4ec8ee3fbff
lieven.hollevoet <lieven.hollevoet@365dfac8-a4c5-11dd-9a14-c1645b63b0c6>
2009-03-21 19:48:24 +0000

    Added sensorhub readout software

---
+++ oswald/software/sensorhub/query_sensorhub.pl
@@ -0,0 +1,253 @@
+#! /usr/bin/perl -w
+
+#########################################################################
+# Script to query and parse sensor readings from the sensorhub
+#
+# Currently supports onewire devices, SHT humidity sensors
+#
+# Lieven Hollevoet, 2009
+#########################################################################
+use lib qw( /opt/lib/perl );
+
+use strict;
+use POSIX;
+#use RRDs;
+use XML::Simple;
+
+use Data::Dumper;
+
+use Getopt::Std;
+
+use IO::Socket;
+use Net::hostent; 
+
+# Register ALARM signal
+$SIG{ALRM} = sub { die "timeout" };
+
+# Global variables
+my $config;
+my $socket;
+
+
+########################## SUBROUTINES  ##########################
+# Read configuration settings from XML file
+sub get_config() {
+	# Read the XML config file
+	my $xml = new XML::Simple(keyattr => ['id'], ForceArray => '1');
+    $config = $xml->XMLin("config.xml");
+	print Dumper($config);
+	
+}
+
+# Parse the response of the sensorhub
+sub parse_report {
+	my $report = shift();
+	
+	
+	# Extract OneWire 
+	while ($report =~ /OneWire\s+([0-9A-F]{8})\s+([0-9A-F]){4}/g){
+		# Store result in RRD database ($id, $value)
+		store_onewire($1, $2);		
+	}
+	 
+}
+
+# Store reading from OneWire sensor in the RRD database defined in the config hash
+sub store_onewire() {
+	my $id    = shift();
+	my $value = shift();
+	
+	my $temperature = hex($value) / 2;
+	my $db;
+	
+	if (exists($config->{'onewire'}->{'node'}->{$id})){
+		$db = $config->{'onewire'}->{'node'}->{$id};
+	} else {
+		print("Unknown OneWire sensor $id\n");
+		return;
+	}
+	
+	print("Storing $temperature in database $db");
+	
+	return;
+	
+}
+
+###############################################################
+## dec2hex
+#   Convert dec number into hex string
+###############################################################
+sub dec2hex() {
+    my $dec  = shift();
+    my $fill = shift();
+
+    my $fmt_string;
+
+    if (defined($fill)){
+	$fmt_string = "%0" . $fill . "X";
+    } else {
+	$fmt_string = "%02X";
+    }
+    return sprintf($fmt_string, $dec );
+}
+
+###############################################################
+## clean_and_exit
+#   Shutdown cleanly
+###############################################################
+sub clean_and_exit() {
+    &close_port();    
+    exit 0;
+}
+
+###############################################################
+## error
+#   Signal error and exit
+###############################################################
+sub error() {
+    my $data = shift();
+    print $data;
+    &close_port();
+    exit 1;
+}
+
+# Returns the date.
+sub the_date() {
+    my ($second, $minute, $hour, $dayOfMonth, $month, $yearOffset, $dayOfWeek, $dayOfYear, $daylightSavings) = localtime();
+    my $year = 1900 + $yearOffset;
+    my $hms  = sprintf("%02i:%02i:%02i", $hour, $minute, $second);
+    my $ymd  = sprintf("%04i%02i%02i", $year, $month, $dayOfMonth);
+    my $theTime = "[$ymd $hms]\n";
+    return $theTime;
+}
+
+
+
+###############################################################
+## close_socket
+#   Close the socket
+###############################################################
+sub close_socket {
+    if (defined $socket){
+		$socket->close();
+    }    
+}
+
+###############################################################
+## open_socket(host, port)
+#   Try to open a TCP session to 'host' on 'port'.
+#   Retry if the server is not available, timeout after 
+#   10 tries with 1 second sleep.
+###############################################################
+sub open_socket {
+
+	# Read input parameters
+    my $client_host = shift(); 
+    my $client_port = shift();
+
+    # Connect to the remote device 
+    # create a tcp connection to the specified host and port
+    my ($kidpid, $handle, $line);
+
+    my $timeout = 0;
+
+    while () {
+        if ($socket = IO::Socket::INET->new(Proto     => "tcp",
+					PeerAddr  => $client_host,
+					PeerPort  => $client_port,
+					Type      => SOCK_STREAM)){
+	   		last;
+        } else {
+	   		print "Waiting for server to become available...\n";
+	   		$timeout++;
+	   		if ($timeout == 10){
+	     		die "Server not available for 10 seconds, exit...\n";
+	   		}
+	   
+	  		sleep 1;
+        }
+    }
+    
+}
+
+###############################################################
+## read_socket(timeout)
+#   Reads data from the socket interface. Times out if nothing is
+#   received after <timeout> seconds.
+###############################################################
+sub read_socket {
+    my $timeout = shift();
+
+    my @numresult;
+    my $result;
+
+    eval {
+	# Set alarm
+	alarm($timeout);
+
+	# Execute receive code
+	my $waiting = 1;
+	$result = "";
+	my $res = "";
+	while ($waiting){
+
+	    # Read reply
+	    ($socket->recv($res, 32));
+	    $result .= $res;
+	    if (($result =~ /EOT/)) {
+			$waiting = 0;
+	    }
+	}
+	
+	# Clear alarm
+	alarm(0);
+    };
+
+    # Check what happened in the eval loop
+    if ($@) {
+	if ($@ =~ /timeout/) {
+	    # Oops, we had a timeout
+	    print "Received up to now: $result";
+		close_socket();
+		die "Response not received in time! Exit...";
+	} else {
+	    # Oops, we died
+	    alarm(0);           # clear the still-pending alarm
+	    die;                # propagate unexpected exception
+	} 
+	
+    } 
+
+    # We get here if the eval exited normally
+    #@numresult = &parse_response($result);
+	return $result;
+	
+}
+
+
+
+
+
+
+
+########################## MAIN PROGRAM ##########################
+
+# Read the config file
+get_config();
+
+# Connect to the remote device
+#open_socket($config->{'hostname'}, $config->{'hostport'});
+
+# Read sensor values, with a timeout of 10 seconds
+#my $report = read_socket(10);
+
+# Close the socket
+#close_socket();
+my $report = "OneWire 00000001 0012\nOneWire 00000002 0013\n";
+
+# Parse
+parse_report($report);
+
+
+
+
