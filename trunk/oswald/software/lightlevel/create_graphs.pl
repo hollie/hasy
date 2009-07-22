ecaee8a8f44915d86f0ef80f48f032ccdc11e7a1
lieven.hollevoet <lieven.hollevoet@365dfac8-a4c5-11dd-9a14-c1645b63b0c6>
2009-07-22 19:48:11 +0000

    Added yearly graph output

--- trunk/oswald/software/lightlevel/create_graphs.pl
+++ trunk/oswald/software/lightlevel/create_graphs.pl
@@ -25,21 +25,23 @@ if (defined($ARGV[0])){
 my $err;
 
 
-# Create the light_level graph
+# Create the light_level graphs
 RRDs::graph "light_days.png",
-    "--end", "now",
     "--start", "end-2days",
-    "--title", "Ambient lightlevel sensornode", 
-    "--vertical-label", "Volt",
-    "--imgformat","PNG",
-    "--lower-limit","0",
+    "--title", "Ambient lightlevel (daily)", 
+    light_cfgstring();
+    ;
+
+if ($err = RRDs::error) {
+    die "ERROR: $err\n";
+};
+
+RRDs::graph "light_year.png",
+    "--start", "end-1year",
+    "--title", "Ambient lightlevel (year)", 
     #"--lazy",
-    "--color","BACK#EEF0F0",
-    "--interlaced",
-    "--watermark","http://bouw.lika.be",
-    "DEF:solar_adc=$light:solar_adc:AVERAGE",
-    "CDEF:solar=solar_adc,255,*,2.5,/,2,*",
-    "LINE:solar#0000FF:Panel output",
+    light_minmax();
+    
     ;
 
 if ($err = RRDs::error) {
@@ -49,7 +51,7 @@ if ($err = RRDs::error) {
 
 print "Figures created...\n";
 
-exit(0);
+#exit(0);
 
 # And upload the stuff to the website
 my $ftp = Net::FTP->new($host, Timeout=>240) or die "Could not create ftp object";
@@ -58,7 +60,8 @@ $ftp->binary;
 
 $ftp->login($ftp_user, $ftp_password) or die "Could not login :", $ftp->message;
 
-&put_graph("ventilatie_week.png");
+&put_graph("light_days.png");
+&put_graph("light_year.png");
 
 $ftp->quit;
 
@@ -68,5 +71,52 @@ sub put_graph()
 {
 	my $name = shift();
 	$ftp->put($name) or die "Could not put $name\n";
+}
+
+# Config string for graphs
+sub light_cfgstring {
+	return  "--end", "now",
+    "--vertical-label", "Volt",
+    "--imgformat","PNG",
+    "--lower-limit","0",
+    "--upper-limit","5",
+    "--rigid",
+    "--color","BACK#EEF0F0",
+    "--interlaced",
+    "--watermark","http://bouw.lika.be",
+    "DEF:solar=$light:solar_adc:AVERAGE",
+    "DEF:vcc=$light:vcc_adc:AVERAGE",
+    "DEF:t_real=$light:temperature:AVERAGE",
+    "CDEF:t_scaled=t_real,10,/",
+    "AREA:solar#FFCCCC",
+    "LINE:solar#990000:Panel output",
+    "LINE:vcc#000099:System voltage",
+    "LINE:t_scaled#009900:Temperature / 10 degrees C",
+    ;    
+}
+sub light_minmax {
+	return  "--end", "now",
+    "--vertical-label", "Volt",
+    "--imgformat","PNG",
+    "--lower-limit","0",
+    "--upper-limit","5",
+    "--rigid",
+    "--color","BACK#EEF0F0",
+    "--interlaced",
+    "--watermark","http://bouw.lika.be",
+    "DEF:vcc_min=$light:vcc_adc:MIN",
+    "DEF:vcc_avg=$light:vcc_adc:AVERAGE",
+    "DEF:vcc_max=$light:vcc_adc:MAX",
+    "DEF:t_real=$light:temperature:AVERAGE",  
+    "DEF:solar=$light:solar_adc:AVERAGE",
+    "CDEF:t_scaled=t_real,10,/",
+    "AREA:vcc_max#CCCCFF",
+    "LINE:vcc_max#990000:Vcc maximum",
+    "LINE:vcc_avg#009900:Vcc average",
+    "AREA:vcc_min#FFFFFF",
+    "LINE:vcc_min#000099:Vcc minimum",
+    "LINE:t_scaled#999999:Temperature / 10 degrees C",
+    "LINE:solar#FFC801:Vsolar average",
+    ;    
 }
 
