13b9332b8fc9162cb8fefe4ca3b515804b47e482
lieven.hollevoet <lieven.hollevoet@365dfac8-a4c5-11dd-9a14-c1645b63b0c6>
2009-12-10 21:10:10 +0000

    Committed code updates from Dirk into main branch, added all commented out sections, code size right now is ~4.5k of a total of 16k.
    Todo: make sure we're using the PLL to run the PIC at a whopping 32 MHz. Probable we'll need to verify this in hardware, as in sim the PLL seems not to be implemented.

--- netnode/firmware/utility_monitor/xpl.h
+++ netnode/firmware/utility_monitor/xpl.h
@@ -1,7 +1,9 @@
 /*************************************************************
 * xPL library
 *
-* (c) 2009, Lieven Hollevoet.
+* (c) 2009
+* Authors: Lieven Hollevoet
+*          Dirk Buekenhoudt
 **************************************************************
 * target device   : PIC18F2320
 * clockfreq       : 8 MHz (internal oscillator)
@@ -16,24 +18,15 @@
 // offset of INSTANCE_ID within target=VENDOR-DEVICEID.
 #define XPL_TARGET_VENDOR_DEVICEID_INSTANCE_ID_OFFSET 22
 
-#define XPL_DEVICE_OFFSET 7
-#define XPL_DEVICE_GAS_VALUE_OFFSET 3
-#define XPL_DEVICE_WATER_VALUE_OFFSET 5
-#define XPL_DEVICE_ELEC_DAY_VALUE_OFFSET 8
-#define XPL_DEVICE_ELEC_NIGTH_VALUE_OFFSET 10
-
-#define XPL_MSG_PART_DEVICE "sensor.basic\n{\ndevice="
-
-
 // INSTANCE_ID must be 16 characters or less !!
 // The offset in EEPROM where the INSTANCE_ID is stored 
 #define XPL_EEPROM_INSTANCE_ID_OFFSET 0x00
-#define RX_BUFSIZE 40
-#define XPL_RXFIFO_SIZE 40
+#define XPL_RX_BUFSIZE 40
+#define XPL_RXFIFO_SIZE 44
 
+// public functions called outside xpl lib
 void xpl_init(void);
 void xpl_handler(void);
-void xpl_addbyte(char);
 void xpl_fifo_push_byte(char);
 
 
