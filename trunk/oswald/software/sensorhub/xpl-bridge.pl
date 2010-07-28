13e5b91592f9c66a3f113d75b6ea3e63a56da748
lieven.hollevoet <lieven.hollevoet@365dfac8-a4c5-11dd-9a14-c1645b63b0c6>
2010-07-28 19:43:15 +0000

    First working version of the bridge. Needs code cleanup and documentation. Ready for first trail run

--- trunk/oswald/software/sensorhub/xpl-bridge.pl
+++ trunk/oswald/software/sensorhub/xpl-bridge.pl
@@ -7,16 +7,17 @@ use XML::Simple;
 use xPL::Client;
 
 # Settings
-my $vendor_id = 'hollie';                # xPL vendor id of this program
-my $device_id = 'sensrhub';              # xPL device id of this program
-my $target    = 'hollie-utilmon.meter';  					 # Target device name
-my $interval  = -5;                   # Time in seconds between two queries of the database files.
+my $vendor_id   = 'hollie';                # xPL vendor id of this program
+my $device_id   = 'sensehub';              # xPL device id of this program
+my $target      = 'mhouse-mh.misterhouse'; # Target device name
+my $interval    = -60;                     # Time in seconds between two queries of the database files.
+my $report_freq = 10;				       # Every sensor value is sent after $report_freq intervals when the value has not changed
 
 
 # Global variables
 my $config;
 my $livedata;
-my $debug = 1;
+my $debug = 0;
 
 # Read configuration settings (map of all available sensors)
 get_config();
@@ -40,6 +41,24 @@ exit(0);
 # Query the meter device
 sub query_database {
 	print the_date() . "Fetching data from RRD databases\n";
+	fetch_livedata();
+	
+	# Check if we need to broadcast new values
+	# A new value is sent whenever the value changes, or after  passes through this loop.
+	my $sensor;
+	foreach $sensor (keys %$livedata){
+		my $shash = $livedata->{$sensor};
+		
+		if ($shash->{'bcast_timer'} < 1){
+			print "Sending $sensor value: $shash->{'value'}\n";
+			$livedata->{$sensor}{'bcast_timer'} = $report_freq;
+			$xpl->send_from_string("-t $target -m xpl-stat -c sensor.basic device=$sensor type=temp current=$shash->{'value'}");
+			sleep(1);
+			}
+	}	
+	print Dumper($livedata) if $debug;
+	
+	print "Done!\n";
 	
 }
 
@@ -65,3 +84,33 @@ sub get_config {
 	$xml=undef;
 }
 
+sub fetch_livedata {
+	
+	my $sensor;
+	my $dbname;
+	my $sensorname;
+	my $sensorvalue;
+	
+	foreach $sensor ( keys %{$config->{'onewire'}{'node'}} ){
+		# Get database name and sensor name
+		$dbname = $config->{'onewire'}{'node'}{$sensor}{'db'};
+		if ($dbname =~ /\/(\w+).rrd/) { $sensorname = $1; } else { $sensorname = 'undefined'};
+		
+		# Get the last value from the database
+		my $data = RRDs::info($dbname);
+		my $err=RRDs::error;
+		if ($err) {print "Problem fetching the info from $dbname: $err\n";}
+     
+     	$sensorvalue = $data->{'ds[temperature].last_ds'};
+     	
+     	# Push name/value pair in live hash, and check if it needs a broadcast of the value
+     	if (!defined($livedata->{$sensorname}) || ($livedata->{$sensorname}{'value'} != $sensorvalue)){
+     		$livedata->{$sensorname}{'bcast_timer'} = 0;
+     	} else {
+     		$livedata->{$sensorname}{'bcast_timer'} -= 1;     	
+     	}
+     	$livedata->{$sensorname}{'value'} = $sensorvalue;
+	}
+	
+	#print Dumper($livedata) if $debug;
+}
