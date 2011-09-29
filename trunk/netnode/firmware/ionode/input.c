0981b67ef8b0b3a667898e5a4fa8c4a4f00bc211
dirk.buekenhoudt@wasilo.be <dirk.buekenhoudt@wasilo.be@365dfac8-a4c5-11dd-9a14-c1645b63b0c6>
2011-09-29 21:58:19 +0000

    Input has been coded. No debouncing has yet been implemented. We check every 20ms if the input has been changed. If this is the case we send xpl msg for input that changed
    Tested in the debugger not yet on the node.
    
    found issue with the processing of messages after interrupt, if we get 2 at the same time we loose a message. This is now nicely solved.
    Also the operator used to reset the bit was wrong, need to use xor

--- trunk/netnode/firmware/ionode/input.c
+++ trunk/netnode/firmware/ionode/input.c
@@ -9,15 +9,119 @@
 * target hardware : NetNode
 *************************************************************/
 
+#include <usart.h>
+#include <delays.h>
+#include <string.h>
+#include <stdio.h>
+
 #include "input.h"
+#include "xpl.h"
+
+unsigned int input_state[MAX_BOARD_INPUTS];  // max inputs = 80 = 8 shift registers
 
 unsigned char input_count = 0;   // number inputs on the extension board - value initialised by eeprom value
 
+void read(void) {
+    unsigned char i = 0; // input_bits
+    unsigned char int_count = 0;
+    unsigned char array_count = 0;
+    
+    // shift current values in the shift register
+    input_ps = 1;
+    Delay10TCYx(1); // delay 312ns
+    input_ps = 0;        
+ 
+    for (i=0; i<input_count; i++) {
+        if (int_count == 16) {
+            array_count++;
+            int_count = 0;        
+        } 
+        
+        input_clk = 1;        
+        Delay10TCYx(1); // delay 312ns
+        
+        input_state[array_count] |= input_data << int_count;
+        
+        input_clk = 0;
+        Delay10TCYx(1); // delay 312ns
+                
+        int_count++; 
+    }
+}    
+
+void input_init() { 
+    unsigned char i = 0;
+    unsigned char int_count = 0;
+    unsigned char array_count = 0;
+    unsigned char mask = 1;
+    unsigned short result;
+    char state[4];
+    char input[10];
+    
+    for (i=0;i<MAX_BOARD_INPUTS;i++) {
+        input_state[i] = 0;
+    }    
+    
+    input_ps = 0;
+    input_clk = 0;
+    
+    // read current inputs
+    read();
+      
+    //send sensor current states    
+    for (i = 0; i<input_count;i++) {
+        if (int_count == 16) {
+            array_count++;
+            int_count = 0;
+            mask = 1;
+        }      
+        
+        result = input_state[array_count] & mask;
+                                                   
+        xpl_send_sensor_basic_input(STAT,"input",((int_count+1) + (16 * array_count)), result);
+                        
+        int_count++; 
+        mask = mask << 1;
+        
+        Delay1KTCYx(32);   // delay 1ms
+    }    
+}    
     
 // send xpl message if input changes state, from high to low or low to high
-
 void input_handler_timer(void) {
+    unsigned int input_state_shadow[MAX_BOARD_INPUTS];     
+    unsigned char i = 0;
+    unsigned char int_count = 0;
+    unsigned char array_count = 0;
+    unsigned char mask = 1;
+    unsigned short result;
+    
     if (input_count > 0) {
+        for (i=0; i<MAX_BOARD_INPUTS; i++) {
+            input_state_shadow[i] = input_state[i];                               
+        }                            
+        // read current state
+        read();
+                        
+        result = input_state_shadow[array_count] ^ input_state[array_count];
         
+        //check delta
+        for (i = 0; i<input_count;i++) {
+            if (int_count == 16) {
+                array_count++;
+                int_count = 0;
+                mask = 1;
+                result = input_state_shadow[array_count] ^ input_state[array_count];
+            }      
+            
+            if (result & mask) {
+                // detected a difference, need to send message
+                xpl_send_sensor_basic_input(STAT,"input",((int_count+1) + (16 * array_count)), (input_state[array_count] & mask) >> int_count);        
+            }                                   
+            int_count++; 
+            mask = mask << 1;
+            
+            Delay1KTCYx(32);   // delay 1ms
+        }                    
     }    
 }    
